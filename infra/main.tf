e1e51c320437e56baaec976638598c84161693f6
Adharsh.U <114822057+adharsh277@users.noreply.github.com>
2025-10-10 13:36:16 +0000

    CICD terrform

--- infra/main.tf
+++ infra/main.tf
@@ -4,17 +4,16 @@ resource "random_integer" "suffix" {
   max = 999
 }
 
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-predictive-maintenance"
-  location = "East Asia"
+# Use existing Resource Group
+data "azurerm_resource_group" "rg" {
+  name = "rg-predictive-maintenance"
 }
 
 # Storage Account
 resource "azurerm_storage_account" "storage" {
   name                     = "pmpstorage${random_integer.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = data.azurerm_resource_group.rg.name
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -22,8 +21,8 @@ resource "azurerm_storage_account" "storage" {
 # IoT Hub
 resource "azurerm_iothub" "iot" {
   name                = "pmpiothub${random_integer.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   sku {
     name     = "S1"
     capacity = 1
@@ -33,16 +32,16 @@ resource "azurerm_iothub" "iot" {
 # Event Hub Namespace
 resource "azurerm_eventhub_namespace" "eh_ns" {
   name                = "pmp-eventhub-ns${random_integer.suffix.result}"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   sku                 = "Standard"
 }
 
 # Container Registry
 resource "azurerm_container_registry" "acr" {
   name                = "pmpregistry${random_integer.suffix.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = true
 }
@@ -50,8 +49,8 @@ resource "azurerm_container_registry" "acr" {
 # AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "pmp-aks"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   dns_prefix          = "pmpaks"
 
   default_node_pool {
